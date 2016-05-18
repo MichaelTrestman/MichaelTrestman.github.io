ee32027e7a1785c633bce53b1a1537a868c3f201
MichaelTrestman <mtrestman@pivotal.io>
2016-05-17 21:28:48 -0700

    chess board with compiled javascripts and stylesheets

--- chessboard/js/pieceFunctions/newPiece.js
+++
@@ -1,6 +0,0 @@
-PieceFunctions.newPiece = function (type, side){
-  this.side = side;
-  this.type = type;
-  this.img = PieceFunction.getImage(type, side);
-  this.moves = PieceFunction.setPieceMoveFromMoveRules(type);
-}
